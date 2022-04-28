6d3f527fb1749d9aa8fc7d0f58c5d71d28fa8b05
Eli <eliezerpayano@hotmail.com>
2022-04-27 22:30:40 -0400

    upload project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {AppSigner} from './app.signer';
+import PSPDFKit from "pspdfkit";
+
+const signing = new AppSigner;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-pspdfkit';
+  title = "PSPDFKit for Web Angular Example";
+
+  ngAfterViewInit() {
+    PSPDFKit.load({
+      // Use the assets directory URL as a base URL. PSPDFKit will download its library assets from here.
+      baseUrl: location.protocol + "//" + location.host + "/assets/",
+      document: "/assets/Document.pdf",
+      container: "#pspdfkit-container",
+      styleSheets: ["/assets/utf8.css"]
+    }).then(async (instance) => {
+      const buttons = {
+          type: "custom",
+          title: "Finish Signing",
+          className: "finish-signing",
+          name: "sign",
+          async onPress() {
+            // When "Finish Signing" is pressed, after the user
+            // has added an ink signature, we proceed to apply
+            // a digital signature to the document. From this
+            // point on the integrity of the file is guaranteed.
+            try {
+              // @ts-ignore
+              await instance.signDocument(null, signing.generatePKCS7);
+              console.log("New signature added to the document!", signing.generatePKCS7);
+            } catch (error) {
+              console.error(error);
+            }
+          },
+      };
+      // For the sake of this demo, store the PSPDFKit for Web instance
+      // on the global object so that you can open the dev tools and
+      // play with the PSPDFKit API.
+      (window as any).instance = await instance;
+     // @ts-ignore
+      instance.setToolbarItems([...PSPDFKit.defaultToolbarItems, buttons])
+
+    });
+  }
 }
--- src/app/app.signer.ts
+++ src/app/app.signer.ts
@@ -1,3 +1,64 @@
-export class Signer{
-  ngAfterInit(){}
+import * as forge from 'node-forge'
+
+export class AppSigner {
+  //Generating the PKCS7 file
+  generatePKCS7 = async (fileContents: string): Promise<ArrayBuffer> =>{
+
+    //I'm taking the files f
+    const certificatePromise = fetch(
+      "./elicert.pem"
+    ).then((response) => response.text());
+    const privateKeyPromise = fetch(
+      "./elikey.pem"
+    ).then((response) => response.text());
+    const [certificatePem, privateKeyPem] = await Promise.all([
+      certificatePromise,
+      privateKeyPromise,
+    ]);
+    const certificate = forge.pki.certificateFromPem(certificatePem);
+    const privateKey = forge.pki.privateKeyFromPem(privateKeyPem);
+
+    const p7 = forge.pkcs7.createSignedData();
+
+    p7.content = new forge.util.ByteStringBuffer(fileContents);
+    p7.addCertificate(certificate);
+    // @ts-ignore
+    // @ts-ignore
+    // @ts-ignore
+    p7.addSigner({
+      key: privateKey,
+      certificate: certificate,
+      digestAlgorithm: forge.pki.oids['sha256'],
+      authenticatedAttributes: [
+        {
+          type: forge.pki.oids['contentType'],
+          value: forge.pki.oids['data'],
+        },
+        {
+          type: forge.pki.oids['messageDigest'],
+        },
+        {
+          type: forge.pki.oids['signingTime'],
+          value: Date.toString(),
+        },
+      ],
+    });
+
+    p7.sign({ detached: true });
+
+    return this.stringToArrayBuffer(forge.asn1.toDer(p7.toAsn1()).getBytes());
+  }
+
+  //This function is used to convert the string to an Array Buffer
+  stringToArrayBuffer(binaryString: string): ArrayBuffer {
+    const buffer = new ArrayBuffer(binaryString.length);
+    let bufferView = new Uint8Array(buffer);
+
+    for (let i = 0, len = binaryString.length; i < len; i++) {
+      bufferView[i] = binaryString.charCodeAt(i);
+    }
+
+    return buffer;
+  }
+
 }
